519235625e01853aff3c637cd25ae62b2d84ce33
Ivan Besarab <IvanVBesarab@gmail.com>
2020-01-18 12:14:45 +0200

    Initial Commit

---
+++ BoostersChallange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoostersChallange
+//
+//  Created by Ivan Besarab on 1/18/20.
+//  Copyright © 2020 Ivan Besarab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
